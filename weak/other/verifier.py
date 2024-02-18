2b24b05900d57542b71ea70636756009b94a65b8
Jianer <congjianer@outlook.com>
2024-02-18 19:15:28 +0800

    Start trying pvm verifier

---
+++ weak/other/verifier.py
@@ -0,0 +1,8 @@
+"""🦜 : What's this?
+
+🐢 : Here is the verifier. It should check whether a file `hi.py` is a valid
+pvm contract. In particular, currently we have the following rules:
+
+1. At the top level, the only allowed statements are `def`, `import` and `from ... import ...`
+
+"""
