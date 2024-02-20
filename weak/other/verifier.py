b9b7fcd5bc9fb123c72e9d6f6498e22df13748fc
Jianer <congjianer@outlook.com>
2024-02-20 18:39:58 +0800

    Start trying to add oneshot commands

--- weak/other/verifier.py
+++ weak/other/verifier.py
@@ -30,9 +30,33 @@ def verify(tree : ast, py_lines_for_debugging: list[str]):
     verify_top_level_statements(tree, py_lines_for_debugging)
     verify_imports(tree, py_lines_for_debugging)
 
-def verify_top_level_statements(tree : ast, py_lines_for_debugging: list[str]):
+
+class S:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    MAG = '\033[93m'
+    RED = '\033[91m'
+    NOR = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def debug_node_str(node: ast.AST, l: list[str]):
+    """
+    l : py_lines_for_debugging
+    """
+    region = '\n'.join(l[node.lineno-1:node.end_lineno])
+    rule = '-----------------------------------------------------------------'
+    return (f'{node.__class__} at line {node.lineno}, col {node.col_offset}').ljust(len(rule), '-') + '\n' + \
+        region + \
+        '\n' + rule + '\n'
+
+def verify_top_level_statements(tree : ast, lines: list[str]):
     for node in tree.body:
         # the top-level nodes
+        if not all([isinstance(node, allowed_node) for allowed_node in Allowed_top_level_statements]):
+            raise AssertionError('Only')
 
-def verify_imports(tree : ast, py_lines_for_debugging: list[str]):
+def verify_imports(tree : ast, lines: list[str]):
     pass
