d7171a5c64ee7a30c39e2a9a3c8f3db37bbe9aa5
Jianer <congjianer@outlook.com>
2024-02-20 16:13:48 +0800

    Start writing more verifier

--- weak/other/verifier.py
+++ weak/other/verifier.py
@@ -1,8 +1,38 @@
-"""🦜 : What's this?
+import ast
 
-🐢 : Here is the verifier. It should check whether a file `hi.py` is a valid
-pvm contract. In particular, currently we have the following rules:
-
-1. At the top level, the only allowed statements are `def`, `import` and `from ... import ...`
+from typing import TextIO
+"""
+The allowed top level statements
+"""
+Allowed_top_level_statements: list[ast.AST] = \
+    [ast.Import, ast.ImportFrom, ast.FunctionDef]
 
 """
+The allowed modules.
+"""
+Allowed_modules : list[str] = ['math', 'cmath', 'typing', 'hashlib', 'hmac']
+
+def verify_and_parse_func(py_file : TextIO) -> ast.AST:
+    """
+    Verifies the source code. source should be the content of a python file.
+    """
+    py_code_content = py_file.read()
+    py_file.seek(0)
+    py_lines_for_debugging = py_file.readlines()
+    tree = ast.parse(py_code_content)
+    verify(tree, py_lines_for_debugging)
+
+"""
+Verify the source code.
+Throws AssertionError if the source code is not valid.
+"""
+def verify(tree : ast, py_lines_for_debugging: list[str]):
+    verify_top_level_statements(tree, py_lines_for_debugging)
+    verify_imports(tree, py_lines_for_debugging)
+
+def verify_top_level_statements(tree : ast, py_lines_for_debugging: list[str]):
+    for node in tree.body:
+        # the top-level nodes
+
+def verify_imports(tree : ast, py_lines_for_debugging: list[str]):
+    pass
