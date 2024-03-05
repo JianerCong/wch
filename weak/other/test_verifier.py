2a89aa6f21ac4d4c1d1a8651a3078103b8377be3
Jianer <congjianer@outlook.com>
2024-03-05 12:15:04 +0800

    Start writing function argument parser

---
+++ weak/other/test_verifier.py
@@ -0,0 +1,109 @@
+import pytest, ast
+from verifier import verify_and_parse_func_str
+
+class S:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    MAG = '\033[93m'
+    RED = '\033[91m'
+    NOR = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def assert_bad_top_level_statements(s: str):
+    # with pytest.raises(AssertionError) as e:
+    with pytest.raises(AssertionError) as e:
+        tree = verify_and_parse_func_str(s)
+
+    print(f'🚮️: captured e.value:{S.CYAN} {e.value} {S.NOR}')
+    assert str(e.value).startswith('verify_top_level_statements')
+
+def assert_ok_verify(s: str):
+    tree = verify_and_parse_func_str(s)
+    assert isinstance(tree, ast.AST)  # passed
+
+def test_hi():
+    s = """
+def hi() -> str:
+    return "hi"
+    """
+    assert_ok_verify(s)
+
+def test_bad_top_levels():
+    s = """
+x = 1
+    """
+    assert_bad_top_level_statements(s)
+
+    s = """
+def hi() -> str:
+    return "hi"
+
+x = 1
+    """
+    assert_bad_top_level_statements(s)
+
+def test_ok_imports():
+    s = """
+import math
+
+def hi() -> str:
+    from math import sin
+    return sin(1.0)
+    """
+    assert_ok_verify(s)
+
+def assert_bad_imports(s: str):
+    with pytest.raises(AssertionError) as e:
+        tree = verify_and_parse_func_str(s)
+
+    print(f'🚮️: captured e.value:{S.CYAN} {e.value} {S.NOR}')
+    assert str(e.value).startswith('verify_imports')
+
+def test_bad_imports():
+    s = """
+import sys
+    """
+    assert_bad_imports(s)
+
+    s = """
+import math
+
+def hi() -> str:
+    from sys import api_version
+    return str(api_version)
+    """
+    assert_bad_imports(s)       # 🦜 : `import sys` is not allowed 
+
+def assert_bad_ids(s: str):
+    with pytest.raises(AssertionError) as e:
+        tree = verify_and_parse_func_str(s)
+
+    print(f'🚮️: captured e.value:{S.CYAN} {e.value} {S.NOR}')
+    assert str(e.value).startswith('verify_identifiers')
+
+def test_bad_verify_ids():
+    s = """
+def hi() -> str:
+    x = open('hi.txt', 'r')
+    s = x.read()
+    x.close()
+    return s
+    """
+
+    assert_bad_ids(s)
+
+    s = """
+def hi() -> str:
+    open = 'hi'
+    return open
+    """
+    assert_bad_ids(s)
+
+    s = """
+def hi(f=open('hi.txt')):
+    return f
+   """
+    assert_bad_ids(s)
--- weak/other/verifier.py
+++ weak/other/verifier.py
@@ -12,6 +12,31 @@ The allowed modules.
 """
 Allowed_modules : list[str] = ['math', 'cmath', 'typing', 'hashlib', 'hmac']
 
+"""
+The following identifiers are considered dangerous and are not allowed in the source code.
+
+    \texttt{\_\_import\_\_} &  import a module \\
+    \texttt{breakpoint} &  enter the debugger \\
+    \texttt{compile} &  compile a string into a code object \\
+    \texttt{eval} &  evaluate a string as a python expression \\
+    \texttt{execfile} & execute a file \\
+    \texttt{exec} &  execute a string as a python statement \\
+    \texttt{get\_ipython} &  get the current IPython instance \\
+    \texttt{globals} &  return the global symbol table \\
+    \texttt{memoryview} & create a memoryview object \\
+    \texttt{help} &  get help on an object \\
+    \texttt{id} & get the identity of an object, usually the memory address.
+    This should be removed because the result is not predictable. \\
+    \texttt{input} &  read a line from the standard input \\
+    \texttt{open} &  open a file \\
+    \texttt{quit / exit} &  exit the interpreter \\
+    \texttt{runfile} &  run a file \\
+    \texttt{vars} & return the \texttt{\_\_dict\_\_} attribute of an object \\
+"""
+Dangerous_identifiers : list[str] = ['__import__', 'breakpoint', 'compile', 'eval', 'execfile', \
+                                     'exec', 'get_ipython', 'globals', 'memoryview', 'help', \
+                                     'id', 'input', 'open', 'quit', 'exit', 'runfile', 'vars']
+
 def verify_and_parse_func(py_file : TextIO) -> ast.AST:
     """
     Verifies the source code. source should be the content of a python file.
@@ -21,15 +46,22 @@ def verify_and_parse_func(py_file : TextIO) -> ast.AST:
     py_lines_for_debugging = py_file.readlines()
     tree = ast.parse(py_code_content)
     verify(tree, py_lines_for_debugging)
+    return tree
 
-"""
-Verify the source code.
-Throws AssertionError if the source code is not valid.
-"""
+def verify_and_parse_func_str(py_code_content : str) -> ast.AST:
+    py_lines_for_debugging = py_code_content.split('\n')
+    tree = ast.parse(py_code_content)
+    verify(tree, py_lines_for_debugging)
+    return tree
+
 def verify(tree : ast, py_lines_for_debugging: list[str]):
+    """
+    Verify the source code.
+    Throws AssertionError if the source code is not valid.
+    """
+    print(f'🦜 : Verifying tree:\n {S.GREEN}{ast.dump(tree, indent=4)}{S.NOR} ')
     verify_top_level_statements(tree, py_lines_for_debugging)
-    verify_imports(tree, py_lines_for_debugging)
-
+    verify_imports_and_ids(tree, py_lines_for_debugging)
 
 class S:
     HEADER = '\033[95m'
@@ -42,9 +74,11 @@ class S:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-def debug_node_str(node: ast.AST, l: list[str]):
+def debug_node_str(node: ast.AST, l: list[str]) -> str:
     """
     l : py_lines_for_debugging
+
+    🦜 : This shows the ast node and the region of the code that it represents.
     """
     region = '\n'.join(l[node.lineno-1:node.end_lineno])
     rule = '-----------------------------------------------------------------'
@@ -55,8 +89,45 @@ def debug_node_str(node: ast.AST, l: list[str]):
 def verify_top_level_statements(tree : ast, lines: list[str]):
     for node in tree.body:
         # the top-level nodes
-        if not all([isinstance(node, allowed_node) for allowed_node in Allowed_top_level_statements]):
-            raise AssertionError('Only')
+        if not any([isinstance(node, allowed_node) for allowed_node in Allowed_top_level_statements]):
+            raise AssertionError(f'verify_top_level_statements: Only `import`, `import from` and `def` are allowed at the top level\n' +  \
+                                 'Error processing:' + debug_node_str(node, lines))
 
-def verify_imports(tree : ast, lines: list[str]):
-    pass
+def verify_imports_and_ids(tree: ast, lines: list[str]):
+    """
+    Verify the imports and the identifiers in the source code.
+
+    🐢 : We do a single walk and verify the imports and the identifiers.
+    """
+    for n in ast.walk(tree):
+        verify_imports(n, lines)
+        verify_identifiers(n, lines)
+
+def verify_imports(n: ast, lines: list[str]):
+    """Verify the imports in the source code.
+
+    🐢 : For now, we walk through all the `import` and `import from` nodes to
+    make sure that only those allowed modules are imported.
+    """
+
+    # print(f'🐢: Verifying node {n}')
+    if isinstance(n, ast.ImportFrom):
+        # 🦜 : module must be in the list
+        if n.module not in Allowed_modules:
+            raise AssertionError(f'verify_imports: Importing from module {n.module} is not allowed.\n' + \
+                             debug_node_str(n, lines))
+    elif isinstance(n, ast.Import):
+        for alias in n.names:
+            if alias.name not in Allowed_modules:
+                raise AssertionError(f'verify_imports: Importing module {alias.name} is not allowed.\n' + debug_node_str(n, lines))
+
+def verify_identifiers(n: ast, lines: list[str]):
+    """Verify the identifiers in the source code.
+
+    🦜 : For now, we simply verify that there's no node with an `id` field and
+    the value of the `id` field is in the list of dangerous identifiers.
+    """
+    if not 'id' in n._fields:
+        return
+    if n.id in Dangerous_identifiers:
+        raise AssertionError(f'verify_identifiers: Identifier `{n.id}` is not allowed.\n' + debug_node_str(n, lines))
