928ac46ebe7a6f18c9b59bed0ca1787d29d76259
Jianer <congjianer@outlook.com>
2024-03-21 18:10:59 +0800

    Start adding type to Tx::Receipt

--- weak/run_scrach_py.zsh
+++ weak/run_scrach_py.zsh
@@ -7,7 +7,7 @@ $w --port 7777 --light-exe
 echo '
 from typing import Any
 def hi(_storage: dict[str, Any], y : int) -> int:
-    _storage["x"] = _storage.get("x", 0) + y
+    _storage["x"] = _storage.get("x", 1) + y
     return _storage["x"]
 ' > /tmp/tmp.py
 
@@ -22,6 +22,22 @@ e=http://localhost
 curl --data $txs $e:7777/add_txs
 
 # deployed address
-deployed_addr="000000000000000000000000ffffffff9a04023e"
-
 curl $e:7777/get_latest_Blk
+
+# invoke the contract set 123
+echo '{
+        "method" : "hi",
+        "args" : {"y" : 122}
+}' > /tmp/tmp.json
+deployed_addr="000000000000000000000000ffffffff9a03fc88"
+txs='[{
+    "from" : "01",
+    "to" : "'"$deployed_addr"'",
+    "data" : "@/tmp/tmp.json",
+    "nonce" : 124,
+    "type" : "python"
+}]
+'
+curl --data $txs $e:7777/add_txs
+h=a46e1806155b08cf95763b2e2dabeebff40859727d07d457d75a23069a66ca15
+curl "$e:7777/get_receipt?hash=$h"
